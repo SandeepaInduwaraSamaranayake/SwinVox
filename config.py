8ef6944d0ead3b5dfee0c64aff475a917bce152d
“SandeepaInduwaraSamaranayake” <sandeepa.20210302@iit.ac.lk>
2025-06-19 12:07:25 +0000

    Commiting LR finder initial implementation

--- config.py
+++ config.py
@@ -136,5 +136,5 @@ __C.TEST.VOXEL_THRESH                       = [.2, .3, .4, .5]
 __C.LR_FINDER                               = edict()
 __C.LR_FINDER.START_LR                      = 1e-7
 __C.LR_FINDER.END_LR                        = 1e-2
-__C.LR_FINDER.NUM_BATCHES_TO_TEST           = 500
+__C.LR_FINDER.NUM_BATCHES_TO_TEST           = 1000
 __C.LR_FINDER.AVG_BETA                      = 0.98
