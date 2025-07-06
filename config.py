3d64c429d7daeb6f446afbb42019d5d868d8917f
“SandeepaInduwaraSamaranayake” <sandeepa.20210302@iit.ac.lk>
2025-07-06 18:17:27 +0000

    Config updated

--- config.py
+++ config.py
@@ -14,20 +14,20 @@ cfg                                         = __C
 __C.DATASETS                                = edict()
 __C.DATASETS.SHAPENET                       = edict()
 
-#__C.DATASETS.SHAPENET.TAXONOMY_FILE_PATH    = './datasets/ShapeNet.json'                                                                                  # for colab original json file
+__C.DATASETS.SHAPENET.TAXONOMY_FILE_PATH    = './datasets/ShapeNet.json'                                                                                  # for colab original json file
 #__C.DATASETS.SHAPENET.TAXONOMY_FILE_PATH    = './datasets/ShapeNet_aeroplane_category.json'                                                                # for colab test json file
-__C.DATASETS.SHAPENET.TAXONOMY_FILE_PATH    = '/kaggle/working/SwinVox/datasets/ShapeNet_aeroplane_category.json'                                          # for kaggle airplane
+#__C.DATASETS.SHAPENET.TAXONOMY_FILE_PATH    = '/kaggle/working/SwinVox/datasets/ShapeNet_aeroplane_category.json'                                          # for kaggle airplane
 # __C.DATASETS.SHAPENET.TAXONOMY_FILE_PATH    = '/kaggle/working/SwinVox/datasets/ShapeNet.json'                                                              # for kaggle full
 
 # __C.DATASETS.SHAPENET.TAXONOMY_FILE_PATH  = './datasets/PascalShapeNet.json'
 
-#__C.DATASETS.SHAPENET.RENDERING_PATH        = '/content/ShapeNetRendering/%s/%s/rendering/%02d.png'                                                         # for colab
-__C.DATASETS.SHAPENET.RENDERING_PATH        = '/kaggle/input/shapenet/ShapeNetRendering/ShapeNetRendering/%s/%s/rendering/%02d.png'                          # for kaggle
+__C.DATASETS.SHAPENET.RENDERING_PATH        = '/content/ShapeNetRendering/%s/%s/rendering/%02d.png'                                                         # for colab
+#__C.DATASETS.SHAPENET.RENDERING_PATH        = '/kaggle/input/shapenet/ShapeNetRendering/ShapeNetRendering/%s/%s/rendering/%02d.png'                          # for kaggle
 
 # __C.DATASETS.SHAPENET.RENDERING_PATH      = '/home/hzxie/Datasets/ShapeNet/PascalShapeNetRendering/%s/%s/render_%04d.jpg'
 
-#__C.DATASETS.SHAPENET.VOXEL_PATH            = '/content/ShapeNetVox32/%s/%s/model.binvox'                                                                   # for colab
-__C.DATASETS.SHAPENET.VOXEL_PATH            = '/kaggle/input/shapenet/ShapeNetVox32/ShapeNetVox32/%s/%s/model.binvox'                                        # for kaggle
+__C.DATASETS.SHAPENET.VOXEL_PATH            = '/content/ShapeNetVox32/%s/%s/model.binvox'                                                                   # for colab
+#__C.DATASETS.SHAPENET.VOXEL_PATH            = '/kaggle/input/shapenet/ShapeNetVox32/ShapeNetVox32/%s/%s/model.binvox'                                        # for kaggle
 
 # __C.DATASETS.PASCAL3D                       = edict()
 # __C.DATASETS.PASCAL3D.TAXONOMY_FILE_PATH    = './datasets/Pascal3D.json'
@@ -61,11 +61,11 @@ __C.CONST.DEVICE                            = '0'
 __C.CONST.RNG_SEED                          = 0
 __C.CONST.IMG_W                             = 224       # Image width for input
 __C.CONST.IMG_H                             = 224       # Image height for input
-__C.CONST.BATCH_SIZE                        = 64        # default is 64. 
+__C.CONST.BATCH_SIZE                        = 32        # default is 64. 
 __C.CONST.N_VIEWS_RENDERING                 = 1         
 __C.CONST.CROP_IMG_W                        = 128       
 __C.CONST.CROP_IMG_H                        = 128       
-__C.CONST.NUM_WORKER                        = 20         # number of data workers -- suggested max is 2, but already initialized 4 as default.
+__C.CONST.NUM_WORKER                        = 16         # number of data workers -- suggested max is 2, but already initialized 4 as default.
 __C.CONST.WEIGHTS                           = '/content/drive/MyDrive/Colab Git Clones/SwinVox/SwinVox/output/checkpoints/2025-06-13T16:07:22.579164/checkpoint-best.pth'   # if training is resuming, uncomment this replace 'path' with weight path.
 
 
@@ -99,7 +99,7 @@ __C.NETWORK.CROSS_ATT_NUM_HEADS             = 4
 #
 __C.TRAIN                                   = edict()
 __C.TRAIN.RESUME_TRAIN                      = False
-__C.TRAIN.NUM_EPOCHS                        = 10           # default 250
+__C.TRAIN.NUM_EPOCHS                        = 200           # default 250
 __C.TRAIN.BRIGHTNESS                        = 0.13746317606570424
 __C.TRAIN.CONTRAST                          = 0.3365401951623921
 __C.TRAIN.SATURATION                        = 0.20370660036548005
